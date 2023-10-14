132c1462201093333a09d53e9280dc949f8ca733
Kayvin <kayvin4@hotmail.com>
2023-10-14 20:59:54 +0200

    feat: add header + logo

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/genplaylist2.svg";
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+	return (
+		<header className="flex xs:flex-row justify-between items-center w-full border-b sm:px-4 p-2 border-gray-500 gap-2">
+			<Link href="/">
+				<Image
+					priority
+					src={Logo}
+					width={200}
+					height={200}
+					alt="Follow us on Twitter"
+				/>
+			</Link>
+			<Button asChild>
+				<Link href="/login">Login</Link>
+			</Button>
+		</header>
+	);
+}
