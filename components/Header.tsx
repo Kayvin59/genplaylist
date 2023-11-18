c25355cc83e6dd278f2f039018d7bcbf47176309
Kayvin <kayvin4@hotmail.com>
2023-11-18 14:06:28 +0100

    fix header image aspect ratio

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,7 @@ export default function Header() {
 	return (
 		<header className="flex xs:flex-row justify-center items-center w-full border-b sm:px-6 p-4 border-gray-500 gap-2">
 			<Link href="/" className="pt-3">
-				<Image
-					priority
-					src={Logo}
-					width={200}
-					height={200}
-					alt="Follow us on Twitter"
-				/>
+				<Image priority src={Logo} width={200} alt="Follow us on Twitter" />
 			</Link>
 			{/* <Button asChild disabled className="font-bold">
 				<Link href="/login">Login</Link>
