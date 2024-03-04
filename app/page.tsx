56f3825e449981b3139800178f11e97187858ea3
kayvin <kayvin4@hotmail.com>
2024-03-04 01:47:55 +0100

    feat: add generate page and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import WaitlistDialog from "@/components/waitlist/WaitlistDialog";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRightSquare } from 'lucide-react';
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -9,7 +11,14 @@ export default function Home() {
 			<h2 className="mb-10 text-lg sm:text-xl text-secondary-foreground font-bold">
 				Share your link and get a playlist, on any streaming platform.
 			</h2>
-			<WaitlistDialog />
+			<Button asChild className="text-lg font-semibold p-5">
+				<Link href="/generate">
+					Generate your playlist now
+					<span className=" ml-4 text-lg font-semibold">
+						<ArrowUpRightSquare />
+					</span>
+				</Link>
+			</Button>
 		</>
 	);
 }
