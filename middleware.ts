1aab2e259eb2a372b737e2a04cdfe76fd93e5c9f
Kayvin59 <kayvin4@hotmail.com>
2024-10-19 03:02:44 +0200

    fix: update clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: ["/", "/generate"],
-});
- 
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
 export const config = {
-  // Protects all routes, including api/trpc.
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
